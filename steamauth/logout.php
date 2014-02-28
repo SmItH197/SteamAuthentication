8894e8117aea14fbe02f43fd79f31f562fffe8f1
Ben Smith <smithben238@gmail.com>
2014-02-28 16:14:14 +0000

    added comment

--- steamauth/logout.php
+++ steamauth/logout.php
@@ -1,5 +1,5 @@
 <?php
-header('Location: ../index.php');
+header('Location: ../index.php'); // Change this to where you want logged out users to be redirected to.
 session_start();
 unset($_SESSION['steamid']);
 

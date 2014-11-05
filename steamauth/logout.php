a42331023526f7b51970a190f5ed049be838618c
Ben Smith <smithben238@gmail.com>
2014-11-05 15:00:23 +0000

    Manually added BlackCetha pull request changes

--- steamauth/logout.php
+++ steamauth/logout.php
@@ -2,6 +2,5 @@
 header('Location: ../'); // Change this to where you want logged out users to be redirected to.
 session_start();
 unset($_SESSION['steamid']);
+unset($_SESSION['steam_uptodate']);
 ?>
-
-

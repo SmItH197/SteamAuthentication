2cfb5834dcd4557f25ac27d3140735680d3ea6a9
Ben Smith <smithben238@gmail.com>
2014-11-05 14:28:00 +0000

    Changed redirect to directory instead of index.php

--- steamauth/logout.php
+++ steamauth/logout.php
@@ -1,5 +1,5 @@
 <?php
-header('Location: ../index.php'); // Change this to where you want logged out users to be redirected to.
+header('Location: ../'); // Change this to where you want logged out users to be redirected to.
 session_start();
 unset($_SESSION['steamid']);
 ?>

d308d43e116ac2b372c02c75a3f50981b505c95a
ZimniY <CyberBrain@users.noreply.github.com>
2015-01-06 15:08:07 +0300

    Update settings.php
    Wrong path - missing slash?

--- steamauth/settings.php
+++ steamauth/settings.php
@@ -8,5 +8,5 @@ $logout_page = ""; // Page to redirect to after a successfull logout (from the r
 if (empty($api_key)) {die("<div style='display: block; width: 100%; background-color: red; text-align: center;'>SteamAuth:<br>Please supply a API-Key!</div>");}
 if (empty($domainname)) {$domainname = "localhost";}
 if ($button_style != "small" and $button_style != "large_no" and $button_style != "large") {$button_style = "large_no";}
-$logout_page = "..".$logout_page;
+$logout_page = "../".$logout_page;
 ?>

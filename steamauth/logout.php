3f5dc05a1b086cec140fa7b7b6468957ecbc7c4a
BigSeeProduction <BigSeeProduction@users.noreply.github.com>
2015-01-09 17:11:47 +0100

    updated header to be relative again
    hope this works... maybe a / is missing but im not sure

--- steamauth/logout.php
+++ steamauth/logout.php
@@ -1,6 +1,7 @@
 <?php
 include("settings.php");
-header("Location: /".$logout_page);
+$parent = dirname($_SERVER['REQUEST_URI']);
+header("Location: $parent/".$logout_page);
 session_start();
 unset($_SESSION['steamid']);
 unset($_SESSION['steam_uptodate']);

36c7cc9d57b25583c26654ad94f515fb62011e30
BlackCetha <blackcetha@gmail.com>
2015-01-10 01:19:46 +0100

    Logout page redirection fix, new features
    Login page, switch to steamauth-array to avoid possible conflicts

--- steamauth/logout.php
+++ steamauth/logout.php
@@ -1,6 +1,6 @@
 <?php
 include("settings.php");
-header("Location: ./".$logout_page);
+header("Location: ../".$steamauth['logoutpage']);
 session_start();
 unset($_SESSION['steamid']);
 unset($_SESSION['steam_uptodate']);

06d45c2fe47657202ce045247d99671b22a8b384
Ben Smith <smithben238@gmail.com>
2016-07-28 14:59:37 +0100

    Updated version number

--- steamauth/SteamConfig.php
+++ steamauth/SteamConfig.php
@@ -1,5 +1,5 @@
 <?php
-//Version 3.1.1
+//Version 3.2
 $steamauth['apikey'] = ""; // Your Steam WebAPI-Key found at http://steamcommunity.com/dev/apikey
 $steamauth['domainname'] = ""; // The main URL of your website displayed in the login page
 $steamauth['logoutpage'] = ""; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!

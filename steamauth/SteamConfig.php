5544a0ae53cc4731bbfb70406f4fc82d9241f507
Segeco <segeco@segeco.me>
2016-12-20 12:46:41 +0000

    Update SteamConfig.php
    Fixed string escape and 500 error.

--- steamauth/SteamConfig.php
+++ steamauth/SteamConfig.php
@@ -6,7 +6,7 @@ $steamauth['logoutpage'] = ""; // Page to redirect to after a successfull logout
 $steamauth['loginpage'] = ""; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
 // System stuff
-if (empty($steamauth['apikey'])) {die("<div style='display: block; width: 100%; background-color: red; text-align: center;'>SteamAuth:<br>Please supply an API-Key!<br>Find this in steamauth/SteamConfig.php, Find the '<b>$steamauth['apikey']</b>' Array. </div>");}
+if (empty($steamauth['apikey'])) {die("<div style='display: block; width: 100%; background-color: red; text-align: center;'>SteamAuth:<br>Please supply an API-Key!<br>Find this in steamauth/SteamConfig.php, Find the '<b>\$steamauth['apikey']</b>' Array. </div>");}
 if (empty($steamauth['domainname'])) {$steamauth['domainname'] = $_SERVER['SERVER_NAME'];}
 if (empty($steamauth['logoutpage'])) {$steamauth['logoutpage'] = $_SERVER['PHP_SELF'];}
 if (empty($steamauth['loginpage'])) {$steamauth['loginpage'] = $_SERVER['PHP_SELF'];}

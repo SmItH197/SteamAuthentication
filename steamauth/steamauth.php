a5221b72c8ca974c7eec344736f617e0be5cda78
guurgle <contact@guurgle.me>
2018-04-06 02:14:32 -0700

    use https

--- steamauth/steamauth.php
+++ steamauth/steamauth.php
@@ -21,14 +21,14 @@ if (isset($_GET['login'])){
 		$openid = new LightOpenID($steamauth['domainname']);
 		
 		if(!$openid->mode) {
-			$openid->identity = 'http://steamcommunity.com/openid';
+			$openid->identity = 'https://steamcommunity.com/openid';
 			header('Location: ' . $openid->authUrl());
 		} elseif ($openid->mode == 'cancel') {
 			echo 'User has canceled authentication!';
 		} else {
 			if($openid->validate()) { 
 				$id = $openid->identity;
-				$ptn = "/^http:\/\/steamcommunity\.com\/openid\/id\/(7[0-9]{15,25}+)$/";
+				$ptn = "/^https?:\/\/steamcommunity\.com\/openid\/id\/(7[0-9]{15,25}+)$/";
 				preg_match($ptn, $id, $matches);
 				
 				$_SESSION['steamid'] = $matches[1];
